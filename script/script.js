13da4b69af1d9f05eb53840ec2da85ed6a723f2a
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2023-05-04 20:11:23 -0300

    04/05/2023

--- script/script.js
+++ script/script.js
@@ -11,7 +11,11 @@ let isValid = false;
 const showDays = document.querySelector('#day');
 const showMonths = document.querySelector('#month');
 const showYears = document.querySelector('#year');
-
+//Label 
+const labelDay = document.querySelector('#labelDay');
+const labelMonth = document.querySelector('#labelMonth');
+const labelYear = document.querySelector('#labelYear')
+//Button
 const submitBttn = document.querySelector('#submitButton');
 
 submitBttn.addEventListener('click', CalculateDT)
@@ -20,15 +24,21 @@ submitBttn.addEventListener('click', CalculateDT)
 showDays.addEventListener('input', (e) => {
     if(showDays.value > 31) {
         error_days.innerHTML = 'Must be a valid date';
+        showDays.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelDay.style.color = 'hsl(0, 100%, 67%)'
         isValid = false;
         return;
     } else {
         isValid = true;
         error_days.innerHTML = '';
+        showDays.style.borderColor = 'hsl(0, 0%, 86%)';
+        labelDay.style.color = 'hsl(0, 1%, 44%)';
     }
     if (+showDays.value === 0) {
         isValid = false;
         error_days.innerHTML = 'This Field is requiered';
+        showDays.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelDay.style.color = 'hsl(0, 100%, 67%)'
         isValid = false;
         return;
     } else {}
@@ -39,15 +49,21 @@ showDays.addEventListener('input', (e) => {
 showMonths.addEventListener('input', (e) => {
     if(showMonths.value > 12) {
         error_months.innerHTML = 'Must be a valid date';
+        showMonths.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelMonth.style.color = 'hsl(0, 100%, 67%)';
         isValid = false;
         return;
     } else {
         isValid = true;
         error_months.innerHTML = '';
+        showMonths.style.borderColor = 'hsl(0, 0%, 86%)';
+        labelMonth.style.color = 'hsl(0, 1%, 44%)';
     }
     if (+showMonths.value === 0) {
         isValid = false;
         error_months.innerHTML = 'This Field is requiered';
+        showMonths.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelMonth.style.color = 'hsl(0, 100%, 67%)';
         isValid = false;
         return;
     } else {}
@@ -58,15 +74,21 @@ showMonths.addEventListener('input', (e) => {
 showYears.addEventListener('input', (e) => {
     if(showYears.value > 2023) {
         erro_years.innerHTML = 'Must be in the past';
+        showYears.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelYear.style.color = 'hsl(0, 100%, 67%)';
         isValid = false;
         return;
     } else {
         isValid = true;
         erro_years.innerHTML = '';
+        showYears.style.borderColor = 'hsl(0, 0%, 86%)';
+        labelYear.style.color = 'hsl(0, 1%, 44%)';
     }
     if (+showYears.value === 0) {
         isValid = false;
         erro_years.innerHTML = 'This Field is requiered';
+        showYears.style.borderColor = 'hsl(0, 100%, 67%)';
+        labelYear.style.color = 'hsl(0, 100%, 67%)';
         isValid = false;
         return;
     } else {}
