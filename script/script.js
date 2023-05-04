83d277b2ec258f34f2ed7006eff993fd00f73cea
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2023-05-03 21:07:55 -0300

    03/05/2023

--- script/script.js
+++ script/script.js
@@ -2,31 +2,74 @@
 const days = document.querySelector('#day-error');
 const months = document.querySelector('#month-error')
 const years = document.querySelector('#year-error')
-const date = [days, months, years]
 // Output
-const ageDays = document.querySelector('#days')
-const ageMonths = document.querySelector('#months')
-const ageYears = document.querySelector('#years')
+const daysResult = document.querySelector('#days')
+const monthsResult = document.querySelector('#months')
+const yearsResult = document.querySelector('#years')
 // Inputs
 const showDays = document.querySelector('#day')
 const showMonths = document.querySelector('#month')
 const showYears = document.querySelector('#year')
 
-function timeOfLiving (birthday) {
-    const now = new Date() 
-    const diff = now - birthday; 
+const TODAY = new Date() 
+const date = date.gateDate();
+const month = 1 + date.getMonth();
+const year = date.getFullYear();
 
-    const day = Math.floor((diff / 1000 / 60 / 60 / 24) % 365.25);
-    const month = Math.floor((diff / 30.4375));
-    const year = Math.floor(diff / 1000 / 60 / 60 / 24 / 365.25);
-}
 
 showDays.addEventListener('input', (dayEvent) => {
     const inputDay = dayEvent.target
     if (inputDay.validity.valid) {
-        const outPutDay = Math.floor((diff / 1000 / 60 / 60 / 24) % 365.25);
-    } else {}
+        
+    } else {
+        if (inputDay.validity.valueMissing) {
+            days.innerHTML = 'This field is requiered';
+        } else if (inputDay.validity.patternMismatch) {
+            days.innerHTML = 'Must be a valid day';
+        } else if (inputElement.validity.typeMismatch || showDays.value > 31) {
+            days.innerHTML = 'Must be a valid date';
+        } else {days.innerHTML = ''}
+    }
 })
+
+showMonths.addEventListener('input', (monthEvent) => {
+    const inputDay = monthEvent.target
+    if (inputDay.validity.valid) {
+        
+    } else {
+        if (inputDay.validity.valueMissing) {
+            months.innerHTML = 'This field is requiered';
+        } else if (inputDay.validity.patternMismatch) {
+            months.innerHTML = 'Must be a valid month';
+        } else if (inputElement.validity.typeMismatch || showMonths.value > 12) {
+            months.innerHTML = 'Must be a valid date';
+        } else {months.innerHTML = ''}
+    }
+})
+
+showYears.addEventListener('input', (yearEvent) => {
+    const inputDay = yearEvent.target
+    if (inputDay.validity.valid) {
+        years.innerHTML = ''
+    } else {
+        if (inputDay.validity.valueMissing) {
+            years.innerHTML = 'This field is requiered';
+        } else if (inputDay.validity.patternMismatch) {
+            years.innerHTML = 'Must be in the past';
+            
+        } else if (yearEvent.value > 2023) {
+            years.innerHTML = 'Must be in the past'
+
+        }else if (inputElement.validity.typeMismatch) {
+            years.innerHTML = 'Must be a valid date';
+
+        } else {years.innerHTML = ''}
+    }
+})
+
+function submit () {
+    window.alert('Oi')
+}
 
 
 
